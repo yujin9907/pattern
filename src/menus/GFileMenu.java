6db4f3146ceb55f4a26b9986d82ecff700122ae6
heo <=>
2025-06-09 15:16:08 +0900

    0606 - 판넬 추가 및 정리 (14주차)

--- src/menus/GFileMenu.java
+++ src/menus/GFileMenu.java
@@ -40,7 +40,7 @@ public class GFileMenu extends JMenu {
         // TODO 수업코드에선 xml 에 상수 정의하고 불러옴 (다음시간에 할 수도 있음)
         // TODO 저장 테스트하고 디폴트 디렉토리 잘 들어가는지 확인
         // TODO 대충 저장해도 .gvs 로 저장되도록
-        this.dlr = new File("/Users/heo/Desktop/univ/패턴중심사고와프로그래밍/pattern-workspace/classes.gvs");
+        this.dlr = new File("./");
     }
 
     public void associate(GDrawingPanel drawingPanel, JTabbedPane jTabbedPane) {
@@ -48,34 +48,42 @@ public class GFileMenu extends JMenu {
         this.jTabbedPane = jTabbedPane;
     }
 
+    private GDrawingPanel getCurrentDrawingPanel() {
+        if (jTabbedPane.getSelectedComponent() instanceof GDrawingPanel currentPanel) {
+            return currentPanel;
+        }
+
+        throw new RuntimeException("판넬이 존재하지 않습니다.");
+    }
 
     public void newPanel() throws IOException {
         System.out.println("new");
 
-        // TODO cancel 이면 동작 안 하도록 해야 됨
         if (!this.close()) return;
 
-        this.drawingPanel.initialize();
+        GDrawingPanel newPanel = new GDrawingPanel();
+        newPanel.initialize();
+        newPanel.seteShapeTool(GConstants.EShapeTool.eSelect);
 
-//        GDrawingPanel newPanel = new GDrawingPanel();
-//        newPanel.seteShapeTool(GConstants.EShapeTool.eSelect);
-//        String tabName = "Untitled " + (jTabbedPane.getTabCount() + 1);
-//        jTabbedPane.addTab(tabName, newPanel);
-//        jTabbedPane.setSelectedComponent(newPanel); // 새 탭으로 전환
-//        jTabbedPane.repaint();
+        String tabName = "Untitled " + (jTabbedPane.getTabCount() + 1);
+        jTabbedPane.addTab(tabName, newPanel);
+        jTabbedPane.setSelectedComponent(newPanel); // 새 탭으로 전환
+        jTabbedPane.repaint();
     }
+
 
     public void open() throws IOException, ClassNotFoundException {
         System.out.println("open");
         FileInputStream fileInputStream = new FileInputStream("file");
         BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
         ObjectInputStream objectInputStream = new ObjectInputStream(bufferedInputStream); // 자바 형태의 버퍼에서 오브젝트로
-        // TODO 이거 select 된 판넬을 가리키도록 수정(여러 판넬 있으면)
-        this.drawingPanel.setShapes(objectInputStream.readObject()); // TODO 타입 캐스트 안에 드가서 화긴
+
+        getCurrentDrawingPanel().setShapes(objectInputStream.readObject()); // 타입 캐스트 안에 드가서 화긴
+
         objectInputStream.close();
     }
 
-    // TODO 5.26 try-catch 여기다가 넣어야 됨
+    // TODO try-catch 여기다가 넣어야 됨
     public boolean save() throws IOException {
         if (this.file == null) {
             boolean returnValue = this.saveAs();
@@ -94,36 +102,41 @@ public class GFileMenu extends JMenu {
         return false;
     }
 
-    public boolean saveAs() throws IOException {
+    public boolean saveAs() {
         boolean bCancel = false;
-        JFileChooser chooser = new JFileChooser(this.dlr);
-        chooser.setSelectedFile(file);
-        // TODO 맥은 확장자 다를 수 있음
-        FileNameExtensionFilter fileNameExtensionFilter = new FileNameExtensionFilter("gvs", "gvs");
-        chooser.setFileFilter(fileNameExtensionFilter);
 
-        int returnVal = chooser.showSaveDialog(this.drawingPanel);
-        if (returnVal == JFileChooser.APPROVE_OPTION) {
-            this.dlr = chooser.getCurrentDirectory();
-            this.file = chooser.getSelectedFile();
-        } else {
-            bCancel = true;
+        try {
+            JFileChooser chooser = new JFileChooser(this.dlr);
+            chooser.setSelectedFile(file);
+            // TODO 맥은 확장자 다를 수 있음
+            FileNameExtensionFilter fileNameExtensionFilter = new FileNameExtensionFilter("gvs", "gvs");
+            chooser.setFileFilter(fileNameExtensionFilter);
+
+            int returnVal = chooser.showSaveDialog(getCurrentDrawingPanel());
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                this.dlr = chooser.getCurrentDirectory();
+                this.file = chooser.getSelectedFile();
+            } else {
+                bCancel = true;
+            }
+        } catch (Exception e) {
+            System.out.println("save as 오류");
         }
+
         return bCancel;
     }
 
     public void print() {
         System.out.println("print");
-
     }
 
     public boolean close() throws IOException {
         System.out.println("close");
         boolean bCancel = false;
 
-        if (this.drawingPanel.isUpdated()) {
+        if (getCurrentDrawingPanel().isUpdated()) {
             int reply = JOptionPane.NO_OPTION;
-            reply = JOptionPane.showConfirmDialog(this.drawingPanel, "변경내용 저장?");
+            reply = JOptionPane.showConfirmDialog(getCurrentDrawingPanel(), "변경내용 저장?");
             if (reply == JOptionPane.CANCEL_OPTION) {
                 bCancel = true;
             } else if (reply == JOptionPane.OK_OPTION) {
